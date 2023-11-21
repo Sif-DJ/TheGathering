1309b6ad63a3aaf91fb9a702f231dccdbee9d43d
Sif Due Jacobsen <due.jacobsen@gmail.com>
2023-11-21 14:17:52 +0100

    Upload Tema 1 package

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import itumulator.executable.Program;
+import itumulator.world.World;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Setup variables
+        int size = 15;
+        int delay = 1000;
+        int display_size = 800;
 
-      
+        // 
+        Program p = new Program(size, display_size, delay);
+        World world = p.getWorld();
+
+
     }
 }
