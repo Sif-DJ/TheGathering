5a24acdb6fac8bc48a2b457f3187545b1b5e9701
TimelessLich <139888197+TimelessLich@users.noreply.github.com>
2023-11-21 15:15:17 +0100

    rabbits in hole

--- src/Tema1/RabbitHole.java
+++ src/Tema1/RabbitHole.java
@@ -2,7 +2,11 @@ package Tema1;
 
 import itumulator.world.NonBlocking;
 
+import java.util.ArrayList;
+
 public class RabbitHole implements NonBlocking {
+
+    ArrayList<Rabbit> rabbits;
 
     public RabbitHole(){}
 
