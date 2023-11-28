a9553b6387ae22fcec1281725df4b9ce085996cb
Sif Due Jacobsen <due.jacobsen@gmail.com>
2023-11-28 15:40:53 +0100

    Small Pack update

--- src/Tema2/Pack.java
+++ src/Tema2/Pack.java
@@ -6,11 +6,16 @@ import java.util.ArrayList;
 
 public class Pack {
     protected ArrayList<Animal> list;
+    protected Animal targetPrey;
     public Pack(){
         list = new ArrayList<>();
     }
 
     public void add(Animal animal){
         list.add(animal);
+    }
+
+    public void assignPrey(Animal animal){
+        targetPrey = animal;
     }
 }
